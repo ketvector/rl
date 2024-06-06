405613defbaa5210ce75c380d42eac712a4d4c53
Neeraj Kumar <kumar7neeraj.yadav@gmail.com>
2024-06-06 14:33:03 +0530

    add todos

--- src/vpg/policy.py
+++ src/vpg/policy.py
@@ -2,6 +2,7 @@ import torch
 
 # function approximator for policy
 class Policy(torch.nn.Module):
+    #TODO: take model input/output at model creation
     def __init__(self):
         super(Policy, self).__init__()
         self.model = torch.nn.Sequential(torch.nn.Linear(4,10), torch.nn.ReLU(), torch.nn.Linear(10,2), torch.nn.Sigmoid(), torch.nn.Softmax())
--- src/vpg/value.py
+++ src/vpg/value.py
@@ -2,6 +2,7 @@ import torch
 
 # approximator for value function to use as baseline reward    
 class BaselineValue(torch.nn.Module):
+    # TODO: take model input/output at model creation
     def __init__(self):
         super(BaselineValue, self).__init__()
         self.model = torch.nn.Sequential(torch.nn.Linear(4, 20), torch.nn.ReLU(), torch.nn.Linear(20,1), torch.nn.ReLU())
